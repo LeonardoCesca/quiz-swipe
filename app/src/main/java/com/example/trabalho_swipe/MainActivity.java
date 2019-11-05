d34bc29efa56dd25c16f29d153ca100581446117
LeonardoCesca <leonardocescaf@gmail.com>
2019-11-05 18:44:56 -0300

    ADD onSwipeTouchListener and create texts to indicate actions

--- app/src/main/java/com/example/trabalho_swipe/MainActivity.java
+++ app/src/main/java/com/example/trabalho_swipe/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.trabalho_swipe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout telaPrincipal;
+    private TextView tvSwipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvSwipe = findViewById(R.id.tvSwipe);
+        telaPrincipal = findViewById(R.id.telaPrincipal);
+
+        telaPrincipal.setOnTouchListener(new OnSwipeTouchListener(this){
+            @Override
+            public void onSwipeBottom() {
+                super.onSwipeBottom();
+                tvSwipe.setText("Para baixo");
+            }
+
+            @Override
+            public void onSwipeTop() {
+                super.onSwipeTop();
+                tvSwipe.setText("Para cima");
+            }
+
+            @Override
+            public void onSwipeLeft() {
+                super.onSwipeLeft();
+                tvSwipe.setText("Para esquerda");
+            }
+
+            @Override
+            public void onSwipeRight() {
+                super.onSwipeRight();
+                tvSwipe.setText("Para direita");
+            }
+        });
     }
 }
